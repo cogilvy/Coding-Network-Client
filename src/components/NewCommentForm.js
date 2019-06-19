d653372179659be3fd22d9f032b4e3ec78790b37
Lorenzo Llamas <ltl924@gmail.com>
2019-06-18 22:45:14 -0400

    fix merge

--- src/components/NewCommentForm.js
+++ src/components/NewCommentForm.js
@@ -22,14 +22,8 @@ class NewCommentForm extends Component {
   render() {
     return (
       <Fragment>
-<<<<<<< HEAD
-
             <textarea id="comment-textarea" type="text" name="comment" style={{width:"500px", marginLeft:"39px"}} placeholder="comment here"></textarea>
             <input id="comment-button" onClick={this.handleClick} style={{position:"absolute", height:"44px"}} type="button" value="Comment !"></input>
-=======
-        <textarea id="comment-textarea" type="text" name="comment" style={{height:"44px", width:"500px", display: "inline-block"}} placeholder="Write your comment here..."></textarea>
-        <input id="comment-button" onClick={this.handleClick} style={{height:"44px", display: "inline-block"}} type="button" value="Comment"></input>
->>>>>>> 749fcd9f7f8d2b773efa20ca34d1399f9217fb2f
       </Fragment>
     );
   }
